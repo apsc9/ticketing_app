5ffe04a40eef2354cbf90f126f0e571ea7608490
aditya <mp7020216@gmail.com>
2024-09-14 23:55:07 +0530

    Intial Commit
    - Auth service basic functionality
    - some basic tests for the functionality
    - Add deployment files for auth services, mongo service and ingress controller

---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+
+const router = express.Router();
+
+router.post('/api/users/signin', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please enter a valid email'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Please enter a valid password')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        const passwordsMatch = await Password.compare(
+            existingUser.password,
+            password
+        )
+
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        //generate a jwt
+
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email
+            },
+            process.env.JWT_KEY!
+        );
+
+        // store it on the session object
+        req.session={
+            jwt: userJwt
+        };
+
+        res.status(200).send(existingUser);
+    
+});
+
+export { router as signinRouter }; 
